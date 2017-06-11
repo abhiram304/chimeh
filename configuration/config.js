2a2dc7216bb00ae1d26109b2dc4bf41a667c5141
abhiram304 <abhiram.304@gmail.com>
2017-06-10 21:14:28 -0700

    first commit

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports={
+/*
+* This file contains the configurations information of Twitter login app.
+* It consists of Twitter app information, database information.
+*/
+
+	"facebook_api_key" 		: 			"1034947163308094",
+	"facebook_api_secret"	:				"0518560d1d18b1140a9b9174873e7161",
+	"callback_url"				: "http://localhost:3000/auth/facebook/callback",
+	"use_database"				:				"true",
+	"host"								:				"chack.cm4wup6plou1.us-west-2.rds.amazonaws.com",
+	"username"						:				"chack",
+	"password"						:				"chack1234",
+	"database"						:				"chack"
+}
